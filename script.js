4eaf29bd3aaedada8660f8c821cefacde5aeccaa
allan <143406881+allanhubifc@users.noreply.github.com>
2023-10-16 01:43:39 +0000

    151023

--- script.js
+++ script.js
@@ -35,26 +35,37 @@ function somar(){
     let valorc3 = parseInt(gebi("resultado3").value)
     let totalalimentos = valorc1 + valorc2 + valorc3
     gebi("totalalimentos").value = totalalimentos
-}
-
-function somardivul(){
     let valord1 = parseInt(gebi("resultado4").value)
     let valord2 = parseInt(gebi("resultado5").value)
     let valord3 = parseInt(gebi("resultado6").value)
     let totaldivul = valord1 + valord2 + valord3
     gebi("totaldivul").value = totaldivul
-}
-
-function somatotal(){
     let valort1 = parseInt(gebi("totalalimentos").value)
     let valort2 = parseInt(gebi("totaldivul").value)
     let total1 = valort1 += valort2  
-    gebi("somatotal").value = "R$ " + total1
+    gebi("somatotal").value =  total1
 }
 
 function moedas(){
-    let dolar = total1 / 5.11
-    let euro = total1 / 5.41
+    let dolar1 = gebi("totalalimentos").value
+    let dolar2 = gebi("totaldivul").value
+    let dolar3 = gebi("somatotal").value
+    let euro1 = gebi("totalalimentos").value
+    let euro2 = gebi("totaldivul").value
+    let euro3 = gebi("somatotal").value
+    let rstdolar1 = dolar1 / 5.05
+    let rstdolar2 = dolar2 / 5.05
+    let rstdolar3 = dolar3 / 5.05
+    let rsteuro1 = euro1 / 5.31
+    let rsteuro2 = euro2 / 5.31
+    let rsteuro3 = euro3 / 5.31
+    gebi("dolar1").value = "$" + rstdolar1
+    gebi("dolar2").value = "$" + rstdolar2
+    gebi("dolar3").value = "$" + rstdolar3
+    gebi("euro1").value = "€" + rsteuro1
+    gebi("euro2").value = "€" + rsteuro2
+    gebi("euro3").value = "€" + rsteuro3
+
 }
 
 function montartexto(){
@@ -78,4 +89,23 @@ function montartexto(){
     + totalCracha + ", vendeu " + qtdCamiseta + " camisetas rendendo R$" + totalCamiseta + "." + "O evento lucrou R$" + pdrAlimentos + " com alimentos" + ", com divulgação lucrou "
     + pdrDivul + " ao total lucrou R$" + pdrTotal
 }
+
+
+function validar(){				
+    var campoNome = gebi("idNome")
+    var valorNome = campoNome.value
+    
+    if(valorNome.length === 0){
+        alert("Preencha o nome!")                   
+        campoNome.style.backgroundColor = rgb(255, 99, 71)
+        campoNome.focus()                          
+        return;
+    }else{
+        campoNome.style.backgroundColor = "white"
+    }
+ 
+}
+
+
+
 
