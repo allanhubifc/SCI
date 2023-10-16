265a31dcd08c9957f233bd2a2b5a21226115856b
allan <143406881+allanhubifc@users.noreply.github.com>
2023-10-16 17:56:56 +0000

    161023

--- script.js
+++ script.js
@@ -84,28 +84,62 @@ function montartexto(){
     let pdrAlimentos = gebi("totalalimentos").value
     let pdrDivul = gebi("totaldivul").value
     let pdrTotal = gebi("somatotal").value
-    gebi("texto").value = "O evento contando com " + inscritos + " inscritos vendeu " + qtdCafe + " cafés rendendo R$" + totalCafe + ", vendeu " + qtdBolo + " bolos rendendo R$" + totalBolo
+    gebi('texto').value = "O evento contando com " + inscritos + " inscritos vendeu " + qtdCafe + " cafés rendendo R$" + totalCafe + ", vendeu " + qtdBolo + " bolos rendendo R$" + totalBolo
     + ", vendeu " + qtdHotdog + " cachorros-quentes  rendendo R$" + totalHotdog + ",  vendeu " + qtdBanner + " benners rendendo R$" + totalBanner + ", vendeu" + qtdCracha + " crachás rendendo R$"
     + totalCracha + ", vendeu " + qtdCamiseta + " camisetas rendendo R$" + totalCamiseta + "." + "O evento lucrou R$" + pdrAlimentos + " com alimentos" + ", com divulgação lucrou "
-    + pdrDivul + " ao total lucrou R$" + pdrTotal
+    + pdrDivul + " ao total lucrou R$" + pdrTotal + " - feito por Allan F.Fantin e Arthur Gaio - "
 }
 
 
-function validar(){				
-    var campoNome = gebi("idNome")
-    var valorNome = campoNome.value
+function validaCPF(cpf) {
+    var Soma = 0
+    var Resto
+  
+    var strCPF = String(cpf).replace(/[^\d]/g, '')
     
-    if(valorNome.length === 0){
-        alert("Preencha o nome!")                   
-        campoNome.style.backgroundColor = rgb(255, 99, 71)
-        campoNome.focus()                          
-        return;
-    }else{
-        campoNome.style.backgroundColor = "white"
-    }
- 
-}
-
+    if (strCPF.length !== 11)
+       return false
+    
+    if ([
+      '00000000000',
+      '11111111111',
+      '22222222222',
+      '33333333333',
+      '44444444444',
+      '55555555555',
+      '66666666666',
+      '77777777777',
+      '88888888888',
+      '99999999999',
+      ].indexOf(strCPF) !== -1)
+      return false
+  
+    for (i=1; i<=9; i++)
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  
+    Resto = (Soma * 10) % 11
+  
+    if ((Resto == 10) || (Resto == 11)) 
+      Resto = 0
+  
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+      return false
+  
+    Soma = 0
+  
+    for (i = 1; i <= 10; i++)
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+  
+    Resto = (Soma * 10) % 11
+  
+    if ((Resto == 10) || (Resto == 11)) 
+      Resto = 0
+  
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+      return false
+  
+    return true
+  }
 
 
 
